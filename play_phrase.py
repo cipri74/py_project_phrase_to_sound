b95091ff1d79b55b30cc54a53a16ac2cbc5f371b
cipri74 <61431797+cipri74@users.noreply.github.com>
2020-12-08 12:34:18 +0200

    Write the structure of the app, no design and functionality

--- play_phrase.py
+++ play_phrase.py
@@ -1,3 +1,15 @@
 """
 GUI to write a sentence and play a speech
 """
+
+from tkinter import Tk, Entry, Button
+
+root = Tk()
+root.title("TextToSpeech")
+e = Entry(root)
+play_button = Button(root, text="Play sentence")
+e.grid(row=0, column=0)
+play_button.grid(row=1, column=0)
+
+if __name__ == '__main__':
+    root.mainloop()
